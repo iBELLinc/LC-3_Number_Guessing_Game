937befee5dd928011cb45fe979a92064372cc40d
Ian <iBELLinc@users.noreply.github.com>
2019-04-19 09:21:44 -0600

    Altered some of the code to be more readable

--- main.asm
+++ main.asm
@@ -1,33 +1,33 @@
-; 				###############################
-;				#	Number Guessing Game	#
+; 				#########################################
+;				#	Number Guessing Game		#
 ; 				#	    A combined work by       	#
 ; 				#	 Cindy Cherry & Ian Bell	#
-; 				###############################
+; 				#########################################
 ;
-;		#############################################################
-;		#			Registers: 						#
-;		#    =================================================	#
-; 		#   R0 = I/O			||	R4 = Tens Digit Sum			#
-; 		#   R1 = Random Number	||	R5 =					#
-; 		#   R2 = Counter		||	R6 = #-110				#
-; 		#   R3				||	R7 = JUMP Addresses		#
-; 		#############################################################
+;		#################################################################
+;		#			Registers: 				#
+;		#    =================================================		#
+; 		#   R0 = I/O			||	R4 = Tens Digit Sum	#
+; 		#   R1 = Random Number	||	R5 =				#
+; 		#   R2 = Counter		||	R6 = #-110		#
+; 		#   R3				||	R7 = JUMP Addresses	#
+; 		#################################################################
 ; 
 ; 
 ; 
 ;
-;			###########################################
+;			#########################################################
 ;			#							#
-;			#		NOTES 	KEY:			#
+;			#		NOTES 	KEY:				#
 ;			#							#
-;			#  ==================================	#
+;			#  ===================================================	#
 ;			#							#
-;			#	#### 	|	Major Boundary		#
-;			#	===	|	Minor Boundary		#
+;			#	#### 	|	Major Boundary			#
+;			#	===	|	Minor Boundary			#
 ;			#	------	|	Sub Boundary	(of # || =)	#
-;			#	~hi~	|	Code Block Description	#
+;			#	~hi~	|	Code Block Description		#
 ;			#							#
-;			###########################################
+;			#########################################################
 ;
 ;
 
@@ -37,17 +37,17 @@
 
 ; #########################################################################
 
-PLAY_AGAIN 	LEA R0, Y		; load ascii value for ‘y’ into R0
+PLAY_AGAIN 		LEA R0, Y		; load ascii value for ‘y’ into R0
 			PUTC			; print ‘y’ to screen
 			JSR LINEBREAK
-ADD R2, R2, #1	; increment random generator counter
+			ADD R2, R2, #1		; increment random generator counter
 
 ; #########################################################################
 
 GENERATOR		; PLACEHOLDER
 			LD R7, RANDNUM
-JSRR R7			; GENERATOR Function Call
-; PLACEHOLDER
+			JSRR R7			; GENERATOR Function Call
+			; PLACEHOLDER
 
 ; #########################################################################
 
@@ -55,23 +55,23 @@ LEA R0, PROMPT_FIRST_GUESS
 PUTS
 
 INPUT_LOOP		GETC
-			STR R0, DIGITS, #0				; get 10s place
+			STR R0, DIGITS, #0			; get 10s place
 			GETC
-			STR R0, DIGITS, #1				; get 1s place
+			STR R0, DIGITS, #1			; get 1s place
 			JSR CALC_TENS				; calculate tens place value
 
-			LDR R0, DIGITS, #1				; reload units into R0
+			LDR R0, DIGITS, #1			; reload units into R0
 			ADD R0, R0, R3				; add units + tens place = R0
 
-									; ~R0 now holds dec value
-									; in binary~
+								; ~R0 now holds dec value
+								; in binary~
 
 
-; TRY_AGAIN		IN 						; take in the user input value
+; TRY_AGAIN		IN 					; take in the user input value
 
 ; --------------------------------------------------------------------------------------------------------------------
 
-; ~Compare value with R1~
+								; ~Compare value with R1~
 
 NOT R0, R0
 ADD R0, R0, #1 		; Negative version of user input
@@ -79,92 +79,92 @@ ADD R0, R1, R0 		; R0 - R1 = R0
 
 ; --------------------------------------------------------------------------------------------------------------------
 
-; IF statements
-BRz CORRECT
-BRn TOO_HIGH
-BRp TOO_LOW
+				; IF statements
+				BRz CORRECT
+				BRn TOO_HIGH
+				BRp TOO_LOW
 
 ; #########################################################################
-; ~Branches~
+								; ~Branches~
 
 TOO_HIGH		LEA R0, PROMPT_TOO_HIGH
 			PUTS
-JSR LINEBREAK
-			BRnzp INPUT_LOOP	; TRY_AGAIN
+			JSR LINEBREAK
+			BRnzp INPUT_LOOP		; TRY_AGAIN
 
 ; =================================================================
 
-TOO_LOW		LEA R0, PROMPT_TOO_LOW
+TOO_LOW			LEA R0, PROMPT_TOO_LOW
 			PUTS
-JSR LINEBREAK
-			BRnzp INPUT_LOOP	; TRY_AGAIN
+			JSR LINEBREAK
+			BRnzp INPUT_LOOP		; TRY_AGAIN
 
 ; =================================================================
 
-CORRECT 		LEA R0, PROMPT_CORRECT		; congratulate user
-PUTS
-JSR LINEBREAK
+CORRECT 		LEA R0, PROMPT_CORRECT				; congratulate user
+			PUTS
+			JSR LINEBREAK
 
-LEA R0, PROMPT_PLAY_AGAIN		; ask user to play again
-PUTS
-BRnzp FIRST_INPUT			; jump to user input LOOP
+			LEA R0, PROMPT_PLAY_AGAIN			; ask user to play again
+			PUTS
+			BRnzp FIRST_INPUT				; jump to user input LOOP
 
-INPUT_ERROR	LEA R0, PROMPT_Y_OR_N
-PUTS
-FIRST_INPUT	GETC						; get user input
+			INPUT_ERROR	LEA R0, PROMPT_Y_OR_N
+			PUTS
+FIRST_INPUT		GETC						; get user input
 
 ; --------------------------------------------------------------------------------------------------------------------
 
 								; ~evaluate user input~
 
-AND R6, R6, #0
-LD R6, N				; load R6 with -110
-ADD R0, R0, R6			; subtract 110 from R0
-BRz DONE				; if (R0 = 0) >> R0 = ‘n’
+			AND R6, R6, #0
+			LD R6, N				; load R6 with -110
+			ADD R0, R0, R6				; subtract 110 from R0
+			BRz DONE				; if (R0 = 0) >> R0 = ‘n’
 
-ADD R0, R0, #-11			; if (R0 = 0)  >> R0 = ‘y’
-BRz PLAY_AGAIN
-BRnzp INPUT_ERROR		; input not valid
+			ADD R0, R0, #-11			; if (R0 = 0)  >> R0 = ‘y’
+			BRz PLAY_AGAIN
+			BRnzp INPUT_ERROR			; input not valid
 
 ; #########################################################################
 
 DONE 	NOT R6, R6, #1
-ADD R0, R0, R6 			; put R0 back to ascii value
-PUTC 					; print ‘n’ to screen
-JSR LINEBREAK
-LEA R0, PROMPT_STOP
-PUTS 					; “Thanks for playing!”
-HALT
+	ADD R0, R0, R6 				; put R0 back to ascii value
+	PUTC 					; print ‘n’ to screen
+	JSR LINEBREAK
+	LEA R0, PROMPT_STOP
+	PUTS 					; “Thanks for playing!”
+	HALT
 
 ; #########################################################################
 
 LINEBREAK		AND R5, R5, #0		; zero R5
-ADD R5, R5, #10		; set R5 to newline char
-			PUTC				; print to screen
+			ADD R5, R5, #10		; set R5 to newline char
+			PUTC			; print to screen
 			RET
-			HALT				; just in case something goes wrong
+			HALT			; just in case something goes wrong
 
 ; #########################################################################
 
 CALC_TENS		LDR R4, DIGITS, #0		; load first digit into R4 as iterator
-			AND R3, R3, #0		; 0 sum out
+			AND R3, R3, #0			; 0 sum out
 			ADD R3, R3, #10
-			ADD R4, R4, #-1		; decrement iterator
+			ADD R4, R4, #-1			; decrement iterator
 			BRnz RET
 			BRnzp CALC_TENS
 
 ; #########################################################################
 
-Y					.FILL #121
-N 					.FILL #-110
-RANDNUM				.FILL x0000		; Generator Address Location
+Y				.FILL #121
+N 				.FILL #-110
+RANDNUM				.FILL x0000							; Generator Address Location
 
-DIGITS				.BLKW #2		; Array for 2 digit number input
+DIGITS				.BLKW #2							; Array for 2 digit number input
 
 PROMPT_FIRST_GUESS 		.STRINGZ “Guess a number 0-100: ”
 PROMPT_TOO_HIGH 		.STRINGZ “Too high. Guess again: ”
-PROMPT_TOO_LOW 		.STRINGZ “Too low. Guess again: ”
-PROMPT_CORRECT 		.STRINGZ “Congratulations! You found the number!”
+PROMPT_TOO_LOW 			.STRINGZ “Too low. Guess again: ”
+PROMPT_CORRECT 			.STRINGZ “Congratulations! You found the number!”
 PROMPT_PLAY_AGAIN 		.STRINGZ “Would you like to play again? (y or n): ”
 PROMPT_Y_OR_N			.STRINGZ “Invalid input! Please enter either ‘y’ or ‘n’: ”
 PROMPT_STOP 			.STRINGZ “Thanks for playing!”
