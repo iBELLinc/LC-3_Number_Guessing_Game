8cbe7da3b0e36b52bdad82b103b46682388738cf
Ian <iBELLinc@users.noreply.github.com>
2019-04-22 13:08:14 -0600

    Tested code with LC-3 simulator
    Everything has been tested and proven to function besides the three JSRR instructions. (Generator and 2 Test_Input instructions)

--- main.asm
+++ main.asm
@@ -8,7 +8,7 @@
 ;		#			Registers: 				#
 ;		#    =================================================		#
 ; 		#   R0 = I/O			||	R4 = Tens Digit Sum	#
-; 		#   R1 = Random Number	||	R5 =				#
+; 		#   R1 = Random Number		||	R5 = DIGITS Pntr	#
 ; 		#   R2 = Counter		||	R6 = #-110		#
 ; 		#   R3				||	R7 = JUMP Addresses	#
 ; 		#################################################################
@@ -51,23 +51,35 @@ GENERATOR		; PLACEHOLDER
 
 ; #########################################################################
 
+			LEA R5, DIGITS
 			LEA R0, PROMPT_FIRST_GUESS
 			PUTS
 
-INPUT_LOOP		GETC
-			STR R0, DIGITS, #0			; get 10s place
+INPUT_LOOP		LD R3, TO_BINARY
 			GETC
-			STR R0, DIGITS, #1			; get 1s place
-			JSR CALC_TENS				; calculate tens place value
+			LEA R7, TEST_INPUT
+			JSRR R7
+			OUT
+			ADD R0, R0, R3
+			STR R0, R5, #0			; get 10s place
+			GETC
+			LEA R7, TEST_INPUT
+			JSRR R7
+			OUT
+			ADD R0, R0, R3
+			STR R0, R5, #1			; get 1s place
+			JSR LINEBREAK
+			JSR CALC_TENS			; calculate tens place value
 
-			LDR R0, DIGITS, #1			; reload units into R0
-			ADD R0, R0, R3				; add units + tens place = R0
+			LDR R0, R5, #1			; reload units into R0
+			ADD R0, R0, R3			; add units + tens place = R0
 
-								; ~R0 now holds dec value
-								; in binary~
+							; ~R0 now holds dec value in ascii~
+			
+			;LD R3, TO_BINARY
+			;ADD R0, R0, R3			; convert R0 to binary
+			AND R3, R3, #0			; reset R3 for next iteration
 
-
-; TRY_AGAIN		IN 					; take in the user input value
 
 ; --------------------------------------------------------------------------------------------------------------------
 
@@ -89,14 +101,12 @@ INPUT_LOOP		GETC
 
 TOO_HIGH		LEA R0, PROMPT_TOO_HIGH
 			PUTS
-			JSR LINEBREAK
 			BRnzp INPUT_LOOP		; TRY_AGAIN
 
 ; =================================================================
 
 TOO_LOW			LEA R0, PROMPT_TOO_LOW
 			PUTS
-			JSR LINEBREAK
 			BRnzp INPUT_LOOP		; TRY_AGAIN
 
 ; =================================================================
@@ -104,69 +114,85 @@ TOO_LOW			LEA R0, PROMPT_TOO_LOW
 CORRECT 		LEA R0, PROMPT_CORRECT				; congratulate user
 			PUTS
 			JSR LINEBREAK
+			JSR LINEBREAK
 
 			LEA R0, PROMPT_PLAY_AGAIN			; ask user to play again
 			PUTS
 			BRnzp FIRST_INPUT				; jump to user input LOOP
 
-			INPUT_ERROR	LEA R0, PROMPT_Y_OR_N
+INPUT_ERROR		JSR LINEBREAK
+			LEA R0, PROMPT_Y_OR_N
 			PUTS
 FIRST_INPUT		GETC						; get user input
+			OUT
 
 ; --------------------------------------------------------------------------------------------------------------------
 
-								; ~evaluate user input~
+											; ~evaluate user input~
 
 			AND R6, R6, #0
-			LD R6, N				; load R6 with -110
-			ADD R0, R0, R6				; subtract 110 from R0
-			BRz DONE				; if (R0 = 0) >> R0 = ‘n’
+			LD R6, N							; load R6 with -110
+			ADD R0, R0, R6							; subtract 110 from R0
+			BRz DONE							; if (R0 = 0) >> R0 = ‘n’
 
-			ADD R0, R0, #-11			; if (R0 = 0)  >> R0 = ‘y’
+			ADD R0, R0, #-11						; if (R0 = 0)  >> R0 = ‘y’
 			BRz PLAY_AGAIN
-			BRnzp INPUT_ERROR			; input not valid
+			BRnzp INPUT_ERROR						; input not valid
 
 ; #########################################################################
 
-DONE 	NOT R6, R6, #1
-	ADD R0, R0, R6 				; put R0 back to ascii value
-	PUTC 					; print ‘n’ to screen
-	JSR LINEBREAK
-	LEA R0, PROMPT_STOP
-	PUTS 					; “Thanks for playing!”
-	HALT
+DONE 		NOT R6, R6
+		ADD R6, R6, #1
+		ADD R0, R0, R6 				; put R0 back to ascii value
+		OUT 					; print ‘n’ to screen
+		JSR LINEBREAK
+		LEA R0, PROMPT_STOP
+		PUTS 					; “Thanks for playing!”
 
 ; #########################################################################
 
-LINEBREAK		AND R5, R5, #0		; zero R5
-			ADD R5, R5, #10		; set R5 to newline char
-			PUTC			; print to screen
-			RET
+LINEBREAK		ST R7, RETURN_FROM_BREAK
+			AND R0, R0, #0		; zero R5
+			ADD R0, R0, #10		; set R5 to newline char
+			OUT			; print to screen
+			LD R7, RETURN_FROM_BREAK
+			JMP R7
 			HALT			; just in case something goes wrong
+RETURN_FROM_BREAK	.BLKW #1
 
 ; #########################################################################
 
-CALC_TENS		LDR R4, DIGITS, #0		; load first digit into R4 as iterator
-			AND R3, R3, #0			; 0 sum out
-			ADD R3, R3, #10
+CALC_TENS		LDR R4, R5, #0			; load first digit into R4 as iterator
+			;LD R3, TO_BINARY
+			;ADD R4, R4, R3			; Convert R4 to ascii
+			BRz RETURN
+			AND R3, R3, #0			; prep sum register
+PLUS_TEN		ADD R3, R3, #10
 			ADD R4, R4, #-1			; decrement iterator
-			BRnz RET
-			BRnzp CALC_TENS
+			BRnz RETURN
+			BRnzp PLUS_TEN
+RETURN			RET
 
 ; #########################################################################
 
 Y				.FILL #121
 N 				.FILL #-110
+TO_BINARY			.FILL #-48
 RANDNUM				.FILL x0000							; Generator Address Location
+TEST_INPUT			.FILL x3150
 
 DIGITS				.BLKW #2							; Array for 2 digit number input
 
-PROMPT_FIRST_GUESS 		.STRINGZ “Guess a number 0-100: ”
-PROMPT_TOO_HIGH 		.STRINGZ “Too high. Guess again: ”
-PROMPT_TOO_LOW 			.STRINGZ “Too low. Guess again: ”
-PROMPT_CORRECT 			.STRINGZ “Congratulations! You found the number!”
-PROMPT_PLAY_AGAIN 		.STRINGZ “Would you like to play again? (y or n): ”
-PROMPT_Y_OR_N			.STRINGZ “Invalid input! Please enter either ‘y’ or ‘n’: ”
-PROMPT_STOP 			.STRINGZ “Thanks for playing!”
+PROMPT_FIRST_GUESS 		.STRINGZ "Guess a two digit number (01-99): "
+
+PROMPT_TOO_HIGH 		.STRINGZ "Too high. Guess again: "
+PROMPT_TOO_LOW 			.STRINGZ "Too low. Guess again: "
+PROMPT_CORRECT 			.STRINGZ "Congratulations! You found the number!"
+
+PROMPT_PLAY_AGAIN 		.STRINGZ "Would you like to play again? (y or n): "
+
+PROMPT_Y_OR_N			.STRINGZ "Invalid input! Please enter either ‘y’ or ‘n’: "
+
+PROMPT_STOP 			.STRINGZ "Thanks for playing!"
 
 .END
